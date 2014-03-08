16b62ee9f880d62b304603f0aa733c83bdbce1cc
Christopher Lovejoy <c@lovejoy.info>
2014-03-08 00:14:25 -0800

    iniital setup

---
+++ README.md
@@ -0,0 +1,4 @@
+Lovejoy.io
+==========
+
+What I've been working on, presented for some reason to you all.
--- bin/init.sh
+++ bin/init.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-project_root=`dirname $0`/..
+cd `dirname $0`/..
+project_root=`pwd`
 cd $project_root
 mkdir -p \
   app \
+  data \
+  public/data \
   public/static/css \
   public/static/js \
   src/app/js/lib \
+  src/data \
   src/public/js/lib \
-  src/public/sass/lib
+  src/public/sass/lib \
+;
 
-cd $project_root/src/public/js
-wget https://raw.github.com/jashkenas/backbone/master/backbone-min.js
+if [[ ! -f $project_root/src/public/js/backbone-min.js ]]; then
+  cd "$project_root/src/public/js"
+  wget https://raw.github.com/jashkenas/backbone/master/backbone-min.js
+fi
+
+if [[ ! -f $project_root/src/public/js/underscore-min.js ]]; then
+  cd "$project_root/src/public/js"
+  wget https://raw.github.com/jashkenas/underscore/master/underscore-min.js
+fi
+
+if [[ ! -f $project_root/data/portfolio.json ]]; then
+  touch "$project_root/data/portfolio.json"
+  cd "$project_root/public/data"
+  ln -s ../../data/portfolio.json
+fi
